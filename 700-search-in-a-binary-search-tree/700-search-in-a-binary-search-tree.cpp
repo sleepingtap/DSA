1dcf77997f3f0837cd0d3c39b902c059aa6b4d62
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-04-14 12:55:30 +0530

    Time: 52 ms (59.49%), Space: 34.9 MB (24.22%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -11,62 +11,40 @@
  */
 class Solution {
 public:
-    // vector<TreeNode*>pre(TreeNode* root)
-    // {
-    //     //vector<TreeNode*>ans;
-    //     if(root==NULL)
-    //     {
-    //         return ans;
-    //     }
-    //     stack<TreeNode*>s;
-    //     s.push(root);
-    //     while(!s.empty())
-    //     {
-    //         TreeNode* temp=s.top();
-    //         s.pop();
-    //         if(temp->right!=NULL)
-    //         {
-    //             s.push(temp->right);
-    //         }
-    //         if(temp->left!=NULL)
-    //         {
-    //             s.push(temp->left);
-    //         }
-    //         ans.push_back(temp);
-    //     }
-    //     return ans;
-    // }
+
     TreeNode* searchBST(TreeNode* root, int val) 
     {
-        //TreeNode* res=root;
-        if(root==NULL)
+        while(root!=NULL && root->val!=val)
         {
-            return NULL;
+            if(root->val>val)
+            {
+                root=root->left;
+            }
+            else
+            {
+                root=root->right;
+            }
         }
-        if(val==root->val)
-        {
-            return root;
-        }
-        else if(val<root->val)
-        {
-             return searchBST(root->left,val);
-        }
-        else// if(val>root->val)
-        {
-            return searchBST(root->right,val);
-        }
-      //  return root;
-//         {
-//             while(root!=val)
-//             {
-//                 root=root->left;
-//                 if(val>root)
-//                 {
-//                     break;
-//                 }
-//             }
-            
-//         }
+        return root;
+    
+        // working code
+        // if(root==NULL)
+        // {
+        //     return NULL;
+        // }
+        // if(val==root->val)
+        // {
+        //     return root;
+        // }
+        // else if(val<root->val)
+        // {
+        //      return searchBST(root->left,val);
+        // }
+        // else
+        // {
+        //     return searchBST(root->right,val);
+        // }
+
         
     }
 };
