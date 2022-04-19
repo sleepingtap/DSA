d1e6d0db3ea66eac2f5274b2bf87fd95ac157e58
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-04-19 12:54:34 +0530

    Time: 99 ms (5.11%), Space: 58.5 MB (15.12%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -0,0 +1,90 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode * first =new TreeNode();
+    TreeNode* second= new TreeNode();
+    void search(TreeNode* root,int x,int y){
+        if(root==NULL )
+            return;
+        search(root->left,x,y);
+        if(root->val==x)
+            first=root;
+        if(root->val==y)
+            second=root;
+        search(root->right,x,y);
+    }
+    
+//     vector<int>in(TreeNode* root)
+//     {
+//         vector<int>ans;
+//         TreeNode* node=root;
+//         stack<TreeNode*>s;
+//         while(true)
+//         {
+//             if(node!=NULL)
+//             {
+//                 s.push(node);
+//                 node=node->left;
+//             }
+//             else
+//             {
+//                 if(s.empty())
+//                 {
+//                     break;
+//                 }
+//                 node=s.top();
+//                 s.pop();
+//                 ans.push_back(root->val);
+//                 node=node->right;
+//             }
+//         }
+//         //cout<<ans;
+//         return ans; 
+        
+//     }
+    void in(TreeNode* root,vector<int> & v1){
+        if(root==NULL)
+            return;
+        in(root->left,v1);
+        v1.push_back(root->val);
+        in(root->right,v1);
+    }
+    void recoverTree(TreeNode* root) 
+    {
+        vector<int>v1,v2;
+        in(root,v1);
+         int x=0;
+         int y=0;
+        v2=v1;
+        sort(v2.begin(),v2.end());
+        for(auto it: v1)
+            cout<<it<<" ";
+        
+        cout<<endl;
+        for(int i=0;i<v1.size()-1;i++)
+        {
+            if(v1[i]!=v2[i])
+            {
+                x=v1[i];
+                y=v2[i];
+                break;
+            }
+        }
+        cout<<x<<" "<<y;
+        search(root,x,y);
+        
+        int temp=first->val;
+        first->val=second->val;
+        second->val =temp;
+    }
+};
