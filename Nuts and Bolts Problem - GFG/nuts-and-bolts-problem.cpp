f97e71ece07e6dacd28b905f610e2cf3f3e5bce8
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-05-13 22:55:00 +0530

    Added solution - LeetHub

---
+++ Nuts and Bolts Problem - GFG/nuts-and-bolts-problem.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+
+	void matchPairs(char nuts[], char bolts[], int n) 
+	{
+	    // code here
+	    vector<int>v1,v2;
+	    sort(nuts,nuts+n);
+	    sort(bolts,bolts+n);
+	   // for(int i=0;i<n;i++)
+	   // {
+	   //     if(nuts[i]==bolts[i])
+	   //     {
+	   //         v1.push_back(nuts[i]);
+	   //         v2.push_back(bolts[i]);
+	   //     }
+	   // }
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        char nuts[n], bolts[n];
+        for (int i = 0; i < n; i++) {
+            cin >> nuts[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> bolts[i];
+        }
+        Solution ob;
+        ob.matchPairs(nuts, bolts, n);
+        for (int i = 0; i < n; i++) {
+            cout << nuts[i] << " ";
+        }
+        cout << "\n";
+        for (int i = 0; i < n; i++) {
+            cout << bolts[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
