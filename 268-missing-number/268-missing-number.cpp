1ed99d6f3d2f53b83d3a22a262f62081020f3190
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-05-28 08:59:19 +0530

    Time: 30 ms (40.13%), Space: 18.6 MB (6.33%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int>temp(n+1,-1);
+        for(int i=0;i<n;i++)
+        {
+            temp[nums[i]]=1;
+        }
+        for(int i=0;i<temp.size();i++)
+        {
+            if(temp[i]==-1)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+};
