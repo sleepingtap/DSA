67fb86b6851727c87efaa0920c34d051629bb9bf
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-05-30 11:40:39 +0530

    Time: 3 ms (67.24%), Space: 5.8 MB (76.09%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int divide(int a, int b) 
+    {
+       if(a==INT_MIN && b==-1)
+       {
+           return INT_MAX;
+       }
+        return (long)a/b;
+        
+    }
+};
