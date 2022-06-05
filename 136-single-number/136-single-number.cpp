ac4ade0b6b5bd1a322cd3a34aedc1987515b851c
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-06-05 19:30:30 +0530

    Time: 40 ms (23.67%), Space: 16.8 MB (87.01%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        int i=0;int k=0;
+        while(i<nums.size())
+        {
+            if(i+1<nums.size() && nums[i]==nums[i+1])
+            {
+                i++;
+            }
+            else
+            {
+                k=nums[i];
+                break;
+            }
+            i++;
+        }
+        return k;
+    }
+};
