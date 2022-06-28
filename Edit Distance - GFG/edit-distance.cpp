ba03243a87e994325068a846aa70a0b4374d9bab
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-06-28 12:07:13 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -5,43 +5,64 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    int f(int i,int j,string s, string t,vector<vector<int>>&dp)
-    {
-        if(i<0)
-        {
-            return j+1;
-        }
-        if(j<0)
-        {
-            return i+1;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(s[i]==t[j])
-        {
-            return 0+f(i-1,j-1,s,t,dp);
-        }
-        else
-        {
-            int a=1+f(i,j-1,s,t,dp);
-            int b=1+f(i-1,j,s,t,dp);
-            int c=1+f(i-1,j-1,s,t,dp);
-            return dp[i][j]=min(a,min(b,c));
-        }
+    // int f(int i,int j,string s, string t,vector<vector<int>>&dp)
+    // {
+    //     if(i<0)
+    //     {
+    //         return j+1;
+    //     }
+    //     if(j<0)
+    //     {
+    //         return i+1;
+    //     }
+    //     if(dp[i][j]!=-1)
+    //     {
+    //         return dp[i][j];
+    //     }
+    //     if(s[i]==t[j])
+    //     {
+    //         return 0+f(i-1,j-1,s,t,dp);
+    //     }
+    //     else
+    //     {
+    //         int a=1+f(i,j-1,s,t,dp);
+    //         int b=1+f(i-1,j,s,t,dp);
+    //         int c=1+f(i-1,j-1,s,t,dp);
+    //         return dp[i][j]=min(a,min(b,c));
+    //     }
         
         
-    }
+    // }
     int editDistance(string s, string t) 
     {
         // Code here
         int n=s.size();
         int m=t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1));
         
-        return f(n-1,m-1,s,t,dp);
-        
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=i;
+        }
+        for(int i=0;i<=m;i++)
+        {
+            dp[0][i]=i;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=min(1+dp[i][j-1],min(1+dp[i-1][j],1+dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
 
