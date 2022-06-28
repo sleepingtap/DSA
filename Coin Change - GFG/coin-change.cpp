0ef652cff442adab21cd654dc6baa00853a6e20f
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-06-28 13:21:51 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,34 +5,58 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    long long int f(int ind,int n,int S[],vector<vector<long long int>>&dp)
-    {
-        if(ind==0)
-        {
-            if(n%S[ind]==0)
-            {
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[ind][n]!=-1)
-        {
-            return dp[ind][n];
-        }
-        long long int nottake=f(ind-1,n,S,dp);
-        long long int take=0;
-        if(S[ind]<=n)
-        {
-            take=f(ind,n-S[ind],S,dp);
-        }
-        return dp[ind][n]=take+nottake;
-    }
+    // long long int f(int ind,int n,int S[],vector<vector<long long int>>&dp)
+    // {
+    //     if(ind==0)
+    //     {
+    //         if(n%S[ind]==0)
+    //         {
+    //             return 1;
+    //         }
+    //         return 0;
+    //     }
+    //     if(dp[ind][n]!=-1)
+    //     {
+    //         return dp[ind][n];
+    //     }
+    //     long long int nottake=f(ind-1,n,S,dp);
+    //     long long int take=0;
+    //     if(S[ind]<=n)
+    //     {
+    //         take=f(ind,n-S[ind],S,dp);
+    //     }
+    //     return dp[ind][n]=take+nottake;
+    // }
     long long int count(int S[], int m, int n) 
     {
         // code here.
-        vector<vector<long long int>>dp(m+1,vector<long long int>(n+1,-1));
-        return f(m-1,n,S,dp);
-        
+        vector<vector<long long int>>dp(m,vector<long long int>(n+1));
+        for(int j=0;j<=n;j++)
+        {
+            if(j%S[0]==0)
+            {
+                dp[0][j]=1;
+            }
+            else
+            {
+                dp[0][j]=0;
+            }
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(j>=S[i])
+                {
+                    dp[i][j]=dp[i][j-S[i]]+dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[m-1][n];
         
     }
 };
