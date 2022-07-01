be7b9fdf81e4f976a5f9b5b3dfd970ee87f91380
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-07-01 10:42:30 +0530

    Added solution - LeetHub

--- Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
@@ -11,65 +11,48 @@ class Solution
 {
     public:
     //Function to find the maximum number of cuts.
-    int solve(int n, int x, int y, int z, vector<int> &dp){
-        if(n == 0) return 0;
-        if(n < 0) return -1;
+    int f(int n, int x, int y, int z,vector<int>&dp)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        if(n<0)
+        {
+            return -1;
+        }
+        if(dp[n]!=0)
+        {
+            return dp[n];
+        }
         
-        if(dp[n]) return dp[n];
+        //int a=0;int b=0;int c=0;
+        int a=f(n-x,x,y,z,dp);
+        int b=f(n-y,x,y,z,dp);
+        int c=f(n-z,x,y,z,dp);
+      
+        dp[n]=1+max(a,max(b,c));
+        if(dp[n]==0)
+        {
+            return dp[n]=-1;
+        }
+        else
+        {
+            return dp[n];
+        }
         
-        int ans1 = solve(n-x, x, y, z, dp);
-        int ans2 = solve(n-y, x, y, z, dp);
-        int ans3 = solve(n-z, x, y, z, dp);
-        dp[n] = 1 + max(max(ans1, ans2), ans3);
-        return dp[n] = (dp[n] == 0)? -1: dp[n];
     }
-    //Function to find the maximum number of cuts.
     int maximizeTheCuts(int n, int x, int y, int z)
     {
         //Your code here
-        vector<int>dp(n+1, 0);
-        int ans = solve(n, x, y, z, dp);
-        return ans == -1? 0: ans;
+        vector<int>dp(n+1,0);
+        int res = f(n,x,y,z,dp);
+        if(res==-1)
+        {
+            return 0;
+        }
+        return res;
     }
-    // int f(int n, int x, int y, int z,vector<int>&dp)
-    // {
-    //     if(n==0)
-    //     {
-    //         return 0;
-    //     }
-    //     if(n<0)
-    //     {
-    //         return -1;
-    //     }
-    //     if(dp[n]!=0)
-    //     {
-    //         return dp[n];
-    //     }
-        
-    //     //int a=0;int b=0;int c=0;
-    //     int a=f(n-x,x,y,z,dp);
-    //     int b=f(n-y,x,y,z,dp);
-    //     int c=f(n-z,x,y,z,dp);
-      
-    //     dp[n]=1+max(a,max(b,c));
-    //     if(dp[n]==0)
-    //     {
-    //         return -1;
-    //     }
-    //     return dp[n];
-        
-    // }
-    // int maximizeTheCuts(int n, int x, int y, int z)
-    // {
-    //     //Your code here
-    //     vector<int>dp(n+1,0);
-    //     int res = f(n,x,y,z,dp);
-    //     if(res==-1)
-    //     {
-    //         return 0;
-    //     }
-    //     return res;
-    // }
 };
 
 // { Driver Code Starts.
