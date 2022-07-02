3528c6a4bfa1bd5eebb27d49676f5c655a1a782b
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-07-02 22:49:31 +0530

    Added solution - LeetHub

--- Count number of hops - GFG/count-number-of-hops.cpp
+++ Count number of hops - GFG/count-number-of-hops.cpp
@@ -8,32 +8,58 @@ class Solution
 {
     public:
     //Function to count the number of ways in which frog can reach the top.
-    long long f(int n,vector<long long> &dp)
-    {
-        if(n<0)
-        {
-            return 0;
-        }
-        if(dp[n]!=-1)
-        {
-            return dp[n]%1000000007;
-        }
-        if(n==1 || n==0)
-        {
-            return 1;
-        }
-        long long int a=f(n-1,dp)%1000000007;
-        long long int b=f(n-2,dp)%1000000007;
-        long long int c=f(n-3,dp)%1000000007;
-        return dp[n]=(a+b+c)%1000000007;
-    }
+    // long long f(int n,vector<long long> &dp)
+    // {
+    //     if(n<0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[n]!=-1)
+    //     {
+    //         return dp[n]%1000000007;
+    //     }
+    //     if(n==1 || n==0)
+    //     {
+    //         return 1;
+    //     }
+    //     long long int a=f(n-1,dp)%1000000007;
+    //     long long int b=f(n-2,dp)%1000000007;
+    //     long long int c=f(n-3,dp)%1000000007;
+    //     return dp[n]=(a+b+c)%1000000007;
+    // }
     long long countWays(int n)
     {
         
         // your code here
-        vector<long long>dp(n+1,-1);
-        return f(n,dp)%1000000007;
+        // vector<long long>dp(n+1,-1);
+        // return f(n,dp)%1000000007;
         
+        
+        long long mod=1000000007;
+        long long first=1;
+        long long sec=2;
+        long long third=4;
+        if(n==1)
+        {
+            return first;
+        }
+        if(n==2)
+        {
+            return sec;
+        }
+        if(n==3)
+        {
+            return third;
+        }
+        long long sum=0;
+        for(int i=4;i<=n;i++)
+        {
+            sum=first%mod+sec%mod+third%mod;
+            first=sec;
+            sec=third;
+            third=sum;
+        }
+        return sum%mod;
     }
 };
 
