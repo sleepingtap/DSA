a7e32b3688e17493a8a75011c3cf7cf86c70630b
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-07-15 12:17:31 +0530

    Time: 19 ms (90.87%), Space: 23.3 MB (48.63%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,65 +1,52 @@
 class Solution {
 public:
-    
-    bool isValid(int i, int j, vector<vector<int>>& grid, int m, int n )
+    bool isvalid(int i,int j,vector<vector<int>>& grid,int n,int m)
     {
-        if(i>=0 && i<m && j>=0 && j<n && grid[i][j] == 1)
+        if(i>=0 && j>=0 && i<n && j<m && grid[i][j]==1)
         {
             return true;
         }
         return false;
     }
-    
-    int dfs(int i, int j,vector<vector<int>>& grid, int m, int n,int &count )
+    void dfs(int i,int j,vector<vector<int>>& grid,int n,int m, int &area)
     {
-        grid[i][j] = 0;
-        count++;
-      //  cout<<i<<" "<<j<<" "<<count<<endl;
+        area++;
+        grid[i][j]=0;
         
-        if(isValid(i+1,j,grid,m,n))
+        if(isvalid(i+1,j,grid,n,m))
         {
-            dfs(i+1,j,grid,m,n,count);
+            dfs(i+1,j,grid,n,m,area);
         }
-        if(isValid(i-1,j,grid,m,n))
+        if(isvalid(i-1,j,grid,n,m))
         {
-            dfs(i-1,j,grid,m,n,count);
+            dfs(i-1,j,grid,n,m,area);
         }
-        if(isValid(i,j+1,grid,m,n))
+        if(isvalid(i,j+1,grid,n,m))
         {
-            dfs(i,j+1,grid,m,n,count);
+            dfs(i,j+1,grid,n,m,area);
         }
-        if(isValid(i,j-1,grid,m,n))
+        if(isvalid(i,j-1,grid,n,m))
         {
-            dfs(i,j-1,grid,m,n,count);
+            dfs(i,j-1,grid,n,m,area);
         }
-        return count;
     }
-    
-    
     int maxAreaOfIsland(vector<vector<int>>& grid) 
     {
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        int ans =0;
-        for(int i=0;i<m;i++)
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                if(grid[i][j] == 0)
+                int area=0;
+                if(grid[i][j]==1)
                 {
-                    continue;
-                }
-                else if(grid[i][j] == 1)
-                {
-                    int count =0;
-                  dfs(i,j,grid,m,n,count);
-                   // cout<<v<<endl;
-                    ans = max(ans, count);
+                    dfs(i,j,grid,n,m,area);
+                    ans=max(ans,area);
                 }
             }
         }
         return ans;
-        
     }
 };
