b9bfe533fc46194268e49ee7131a948e3c8690c0
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-07-16 21:05:26 +0530

    Time: 14 ms (61.44%), Space: 11.6 MB (11.93%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int mx = 1e9+7;
+   // int mx = 1e9+7;
    long long int f( int i, int j,int maxMove,int m, int n,vector<vector<vector<long long int>>>&dp)
     {
         if(i<0 || j<0 || i==m || j==n)
@@ -15,10 +15,10 @@ public:
         {
             return dp[maxMove][i][j];
         }
-       long long int a=f(i-1,j,maxMove-1,m,n,dp)%mx;
-       long long int b=f(i+1,j,maxMove-1,m,n,dp)%mx;
-        long long int c=f(i,j+1,maxMove-1,m,n,dp)%mx;
-        long long int d=f(i,j-1,maxMove-1,m,n,dp)%mx;
+        long long int a=f(i-1,j,maxMove-1,m,n,dp)%1000000007;
+        long long int b=f(i+1,j,maxMove-1,m,n,dp)%1000000007;
+        long long int c=f(i,j+1,maxMove-1,m,n,dp)%1000000007;
+        long long int d=f(i,j-1,maxMove-1,m,n,dp)%1000000007;
         return dp[maxMove][i][j]=(a%1000000007+b%1000000007+c%1000000007+d%1000000007)%1000000007;
     }
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) 
