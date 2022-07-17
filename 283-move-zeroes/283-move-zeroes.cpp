8e2183aad02d49eba8e7cf018801f5a6984c3d91
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-07-18 01:29:29 +0530

    Time: 20 ms (96.16%), Space: 19.2 MB (83.28%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int i=0;int j=1;
+        while(i<n && j<n)
+        {
+            if(nums[i]==0 && nums[j]!=0)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+            else if(nums[i]!=0 && nums[j]==0)
+            {
+                i++;
+                j++;
+            }
+            else if(nums[i]==0 && nums[j]==0)
+            {
+                j++;
+            }
+            else if(nums[i]!=0 && nums[j]!=0)
+            {
+                i++;
+                j++;
+            }
+        }
+        
+    }
+};
