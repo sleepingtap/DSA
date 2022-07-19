6e5a034a6bb8c2c286cb067d7ffcf3a4f68d5781
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-07-19 21:31:17 +0530

    Time: 6 ms (70.19%), Space: 9.6 MB (65.95%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -5,10 +5,10 @@ public:
         int n=arr.size();
         int l=0;
         int h=n-1;
-        if(arr[0]>k)
+       /* if(arr[0]>k)
         {
             return k;
-        }
+        }*/
         while(l<=h)
         {
             //cout<<arr[l]<<" "<<l;
@@ -27,15 +27,15 @@ public:
         // if(l>h)
         // {
            // cout<<l<<" "<<h;
-            if(arr[h]-(h+1)<k)
+          /*  if(arr[h]-(h+1)<k)
             {
-                return arr[h]+(k-(arr[h]-(h+1)));
-            }
+                return arr[h]+(k-(arr[h]-(h+1)));*/
+            //}
             // else
             // {
             //     return k;
             // }
         // }
-        return 0;
+        return l+k;
     }
 };
