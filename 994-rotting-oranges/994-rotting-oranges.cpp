497a40e7b2a2e31c1538a30ba29e78e7c5a6fdc0
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-07-27 15:55:10 +0530

    Time: 14 ms (40.61%), Space: 13.1 MB (79.07%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -23,39 +23,29 @@ public:
             int p=q.size();
             while(p--)
             {
-            int i=q.front().first;
-            int j=q.front().second;
-            int f1=0,f2=0,f3=0,f4=0;
-           // int p=q.size();
-            q.pop();
-            if(i+1<n && grid[i+1][j]==1)
-            {
-                grid[i+1][j]=2;
-                q.push({i+1,j});
-                cout<<i+1<<" "<<j<<endl;
-                f1=1;
-            }
-            if(i-1>=0 && grid[i-1][j]==1)
-            {
-                grid[i-1][j]=2;
-                q.push({i-1,j});
-                 cout<<i-1<<" "<<j<<endl;
-                f2=1;
-            }
-            if(j+1<m && grid[i][j+1]==1)
-            {
-                grid[i][j+1]=2;
-                q.push({i,j+1});
-                 cout<<i<<" "<<j+1<<endl;
-                f3=1;
-            }
-            if(j-1>=0 && grid[i][j-1]==1)
-            {
-                grid[i][j-1]=2;
-                q.push({i,j-1});
-                 cout<<i<<" "<<j-1<<endl;
-                f4=1;
-            }
+                int i=q.front().first;
+                int j=q.front().second;
+                q.pop();
+                if(i+1<n && grid[i+1][j]==1)
+                {
+                    grid[i+1][j]=2;
+                    q.push({i+1,j});
+                }
+                if(i-1>=0 && grid[i-1][j]==1)
+                {
+                    grid[i-1][j]=2;
+                    q.push({i-1,j});
+                }
+                if(j+1<m && grid[i][j+1]==1)
+                {
+                    grid[i][j+1]=2;
+                    q.push({i,j+1});
+                }
+                if(j-1>=0 && grid[i][j-1]==1)
+                {
+                    grid[i][j-1]=2;
+                    q.push({i,j-1});
+                }
             }
             if(!q.empty())
             {
