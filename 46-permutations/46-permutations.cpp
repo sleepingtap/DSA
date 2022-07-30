7bf1e8cacd401a8adfe5a7c9513136c1ec3ea61f
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-07-30 13:07:31 +0530

    Time: 6 ms (46.68%), Space: 8 MB (27.62%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void per(int index,vector<int> nums,vector<vector<int>> &ans)
+    void f(int index,vector<int> nums,vector<vector<int>>&ans)
     {
         if(index==nums.size())
         {
-              ans.push_back(nums);
+            ans.push_back(nums);
+            return;
         }
         for(int i=index;i<nums.size();i++)
         {
             swap(nums[index],nums[i]);
-            per(index+1,nums,ans);
-         //   swap(nums[index],nums[i]);
-        }    
+            f(index+1,nums,ans);
+        }
     }
     vector<vector<int>> permute(vector<int>& nums) 
     {
         vector<vector<int>>ans;
-        per(0,nums,ans);
+        f(0,nums,ans);
         return ans;
     }
 };
