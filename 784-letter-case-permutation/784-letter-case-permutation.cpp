c8648e0ce2fc69e025e331c4fb721df71b762ab1
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-08-01 11:35:16 +0530

    Time: 4 ms (92.29%), Space: 11 MB (63.43%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void f(int index,string &s,vector<string>&ans)
+    {
+        //ans.push_back(s);
+        if(index==s.size())
+        {
+            ans.push_back(s);
+            return;
+        }
+        
+        if(isalpha(s[index]))
+        {
+            
+            if(isupper(s[index]))
+            {
+                s[index]=toupper(s[index]);
+                f(index+1,s,ans);
+                s[index]=tolower(s[index]);
+            }
+            else
+            {
+                s[index]=tolower(s[index]);
+                f(index+1,s,ans);
+                s[index]=toupper(s[index]);
+            }  
+            
+        }
+        f(index+1,s,ans);
+
+    }
+    vector<string> letterCasePermutation(string s) 
+    {
+        vector<string>ans;
+        f(0,s,ans);
+        return ans;
+    }
+};
