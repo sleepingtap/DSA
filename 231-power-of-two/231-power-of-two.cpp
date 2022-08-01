eff623ec5cfec50d3fe7d92e109666cb1653ce1e
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-08-01 16:26:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.52%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) 
     {
-        if(n==0)
+        if(n<0)
         {
             return false;
         }
-        while(n!=1)
-        {
-            if(n%2!=0)
-            {
-                return false;
-            }
-            n=n/2;
-            
-        }
-        return true;
-
+        bitset<32>bt(n);
+        int c=0;
         
+        for(int i=0;i<bt.size();i++)
+        {
+            //cout<<bt[i]<<" ";
+            if(bt[i]==1)
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            return true;
+        }
+        return false;
     }
 };
