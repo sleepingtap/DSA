67e3cb6b839ef6d8892de3981e2fbdc1f77e4cd9
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-08-01 13:41:40 +0530

    Time: 6 ms (84.08%), Space: 9.1 MB (7.97%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
+    int f(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp)
+    {
+        if(i==triangle.size()-1)
+        {
+            return triangle[i][j];
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int l=triangle[i][j]+f(i+1,j,triangle,dp);
+        int r=triangle[i][j]+f(i+1,j+1,triangle,dp);
+      
+        return dp[i][j]=min(l,r);
+    }
     int minimumTotal(vector<vector<int>>& triangle) 
     {
-        
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n));
-        dp[0][0]=triangle[0][0];
-        for(int j=1;j<n;j++)
-        {
-            dp[0][j]=10009;
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                int l=10009;
-                if(j-1>=0)
-                {
-                    l=dp[i-1][j-1];
-                }
-                if(j<i+1)
-                {
-                    dp[i][j]=(min(dp[i-1][j],l)+triangle[i][j]);
-                }
-                else
-                {
-                    dp[i][j]=10009;
-                }
-            }
-        }
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        int ans=dp[n-1][0];
-        for(int j=1;j<n;j++)
-        {
-            ans=min(ans,dp[n-1][j]);
-        }
-        return ans;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return f(0,0,triangle,dp);
     }
 };
