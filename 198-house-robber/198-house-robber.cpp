c3601970198555a1579b604d18f8868d1c5871e8
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-08-01 12:21:29 +0530

    Time: 358 ms (7.64%), Space: 344.4 MB (6.07%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int f(int index,int sum,vector<int>& nums,int ans,vector<vector<int>>&dp)
+    {
+        if(index>=nums.size())
+        {
+            ans=max(ans,sum);
+            return ans;
+        }
+        if(dp[index][sum]!=-1)
+        {
+            return dp[index][sum];
+        }
+        int l=f(index+2,sum+nums[index],nums,ans,dp);
+        int r=f(index+1,sum,nums,ans,dp);
+        return dp[index][sum]=max(l,r);
+    }
+    
+    int rob(vector<int>& nums) 
+    {
+        vector<vector<int>>dp(nums.size()+1,vector<int>(100000,-1));
+        return f(0,0,nums,0,dp);
+    }
+};
