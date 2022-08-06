eef1ed4824972b3bdc1fdbd9db708f529e78e49b
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-08-06 15:47:13 +0530

    Time: 314 ms (5.34%), Space: 11.8 MB (54.95%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -31,7 +31,6 @@ public:
         ListNode* c=head;
         while(k--)
         {
-           
             ListNode* c1=c;
             ListNode* prev=NULL;
             while(c->next!=NULL)
@@ -42,52 +41,6 @@ public:
             c->next=c1;
             prev->next=NULL;
         }
-        return c;
-//         ListNode* c=head;ListNode* c1=head;
-//         ListNode* t=NULL;
-//         int a=0;
-//         while(c!=NULL)
-//         {
-//             a++;
-//             c=c->next;
-//         }
-//         if(k==a)
-//         {
-//             return head;
-//         }
-//         while(c1->next!=NULL)
-//         {
-//             t=c1;
-//             c1=c1->next;
-//         }
-//         while(k>a)
-//         {
-//             k=k-a;
-//         }
-//         cout<<k;
-        
-//         ListNode* curr=head;
-//         ListNode* prev=NULL;
- 
-//         for(int i=0;i<=k;i++)
-//         {
-//             prev=curr;
-//             curr=curr->next;
-//         }
-//         if(curr==NULL)
-//         {
-//             prev->next=head;
-//             t->next=NULL;
-//             return prev;
-//         }
-//         ListNode* temp=curr;
-//         while(temp->next!=NULL)
-//         {
-//             temp=temp->next;
-//         }
-//         temp->next=head;
-//         prev->next=NULL;
-//         return curr;
-        
+        return c;        
     }
 };
