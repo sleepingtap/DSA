99d9bd624837ab45e515559939610f4b87912e57
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-08-15 11:42:53 +0530

    Time: 18 ms (34.01%), Space: 5.8 MB (91.63%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    string intToRoman(int a) 
+    {
+        string ans="";
+        //int a=num;
+        while(a>=1000)
+        {
+            ans+='M';
+            a-=1000;
+        }
+        if(a>=900)
+        {
+            ans+="CM";
+            a-=900;
+        }
+        while(a>=500)
+        {
+            ans+='D';
+            a-=500;
+        }
+        if(a>=400)
+        {
+            ans+="CD";
+            a-=400;
+        }
+        while(a>=100)
+        {
+            ans+='C';
+            a-=100;
+        }
+        if(a>=90)
+        {
+            ans+="XC";
+            a-=90;
+        }
+        while(a>=50)
+        {
+            ans+='L';
+            a-=50;
+        }
+        if(a>=40)
+        {
+            ans+="XL";
+            a-=40;
+        }
+        while(a>=10)
+        {
+            ans+='X';
+            a-=10;
+        }
+        if(a>=9)
+        {
+            ans+="IX";
+            a-=9;
+        }
+        while(a>=5)
+        {
+            ans+='V';
+            a-=5;
+        }
+        if(a>=4)
+        {
+            ans+="IV";
+            a-=4;
+        }
+        while(a>=1)
+        {
+            ans+='I';
+            a-=1;
+        }
+        return ans;
+    }
+};
