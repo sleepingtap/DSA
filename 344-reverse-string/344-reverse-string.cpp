6d18918a647324e5c5f111d850aa1500b3777d6d
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-08-29 23:11:49 +0530

    Time: 38 ms (43.10%), Space: 23.2 MB (41.08%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
+    void r(vector<char>&s1,int i,int j)
+    {
+        if(i>=j)
+            return;
+        swap(s1[i],s1[j]);
+        r(s1,i+1,j-1);
+    }
     void reverseString(vector<char>& s) 
     {
-        int l=0;
-        int h=s.size()-1;
-        while(l<=h)
-        {
-            swap(s[l],s[h]);
-            l++;
-            h--;
-        }
+        
+        return r(s,0,s.size()-1);
+        // int l=0;
+        // int h=s.size()-1;
+        // while(l<=h)
+        // {
+        //     swap(s[l],s[h]);
+        //     l++;
+        //     h--;
+        // }
         
     }
 };
