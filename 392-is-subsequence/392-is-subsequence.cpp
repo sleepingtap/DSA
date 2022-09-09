065409783746e273a9020429a9f1585e3ffb4446
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-09-09 22:20:13 +0530

    Time: 2 ms (58.45%), Space: 6.3 MB (88.15%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -2,45 +2,49 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) 
     {
-//         int it_s;
-//         for(int i=0;i<t.size();i++)
-//         {
+        int j=0;
+        for(int i=0;i<t.size();i++)
+        {
+            if(s[j]==t[i])
+            {
+                j++;
+                if(j==s.size())
+                {
+                    break;
+                }
+            }
             
-//         }
-        // if(t.size()==1)
-        // {
-        //     if(s.size()==1)
-        //     {
-        //         if(s[0]==t[0])
-        //             return true;
-        //         return false;
-        //     }  
-        // }
-        queue<char>q;
-        char ch='#';
-        int iterate_t=0;
-        for(int i=0;i<s.size();i++)
-        {
-            q.push(s[i]);
         }
-        while(iterate_t<t.size() && !q.empty())
+        if(j!=s.size())
         {
-            if(ch=='#')
-            {
-                ch=q.front();
-                // q.pop();
-            }
-            if(ch==t[iterate_t])
-            {
-                ch='#';
-                q.pop();
-            }
-            iterate_t++;
+            return false;
         }
-        if(q.empty())
-        {
-            return true;
-        }
-        return false;
+        return true;
     }
+        // queue<char>q;
+        // char ch='#';
+        // int iterate_t=0;
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     q.push(s[i]);
+        // }
+        // while(iterate_t<t.size() && !q.empty())
+        // {
+        //     if(ch=='#')
+        //     {
+        //         ch=q.front();
+        //     }
+        //     if(ch==t[iterate_t])
+        //     {
+        //         ch='#';
+        //         q.pop();
+        //     }
+        //     iterate_t++;
+        // }
+        // if(q.empty())
+        // {
+        //     return true;
+        // }
+        // return false;
+    
 };
