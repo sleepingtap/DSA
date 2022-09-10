3f8524e954bb705f1863a9c5d5efe2d2b761e891
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-09-10 08:54:10 +0530

    Time: 679 ms (20.24%), Space: 134.7 MB (61.82%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+    {
+        vector<int>max_slid_window;
+        deque<int>maximums;
+        int minofwindow=0;
+        int maxofwindow=0;
+        while(maxofwindow<nums.size())
+        {
+            while(maximums.size()>0 && nums[maxofwindow]>maximums.back())
+            {
+                maximums.pop_back();
+            }
+            maximums.push_back(nums[maxofwindow]);
+            if(maxofwindow-minofwindow+1<k)
+            {
+                maxofwindow++;
+            }
+            else if(maxofwindow-minofwindow+1==k)
+            {
+                max_slid_window.push_back(maximums.front());
+                if(nums[minofwindow]==maximums.front())
+                {
+                    maximums.pop_front();
+                }
+                minofwindow++;
+                maxofwindow++;
+            }
+        }
+        return max_slid_window;
+        
+    }
+};
