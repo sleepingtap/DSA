e9f9b5a5756e3e5b6b1bdc3d356ae177d4b9d574
SONAL BANSAL <97297365+sleepingtap@users.noreply.github.com>
2022-10-13 13:35:00 +0530

    Added solution - LeetHub

--- Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
+++ Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
@@ -1,11 +1,11 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial template for C++
 
 #include<bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
 
 
@@ -13,30 +13,37 @@ class Solution
 {
     public:
     //Function to find length of shortest common supersequence of two strings.
+    int f(int i,int j,string X, string Y, int m, int n,vector<vector<int>>&dp)
+    {
+        if(i==m || j==n)
+        {
+            return 0;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int l=0;int r=0;
+        if(X[i]==Y[j])
+        {
+            l=1+f(i+1,j+1,X,Y,m,n,dp);
+        }
+        else
+        {
+            r=max(f(i,j+1,X,Y,m,n,dp),f(i+1,j,X,Y,m,n,dp));
+        }
+        return dp[i][j]=max(l,r);
+    }
     int shortestCommonSupersequence(string X, string Y, int m, int n)
     {
         //code here
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(X[i-1]==Y[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
-                }
-            }
-        }
-        
-        return m+n-dp[m][n];
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        int ans=f(0,0,X,Y,m,n,dp);
+        return m+n-ans;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main()
 {   
@@ -57,4 +64,5 @@ int main()
 	return 0;
 }
 
-  // } Driver Code Ends
+
+// } Driver Code Ends
